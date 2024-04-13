c23c0cdb8f0f15c49a7f6b9129ca5a463cdb08d6
Caio Costa <caiopaulinocosta@hotmail.com>
2024-04-12 21:48:32 -0300

    Adiciona estilos a Header, Footer

--- minha-agenda/src/App.tsx
+++ minha-agenda/src/App.tsx
@@ -4,11 +4,15 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteúdo...</h1>
+      <main className={styles.main}>
+        <h1>Content...</h1>
+      </main>
       <Footer />
     </div>
   );
