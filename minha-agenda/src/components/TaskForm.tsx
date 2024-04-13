2282cd5a7905699cf0f51a9530d7eedff422eee5
Caio Costa <caiopaulinocosta@hotmail.com>
2024-04-12 22:04:44 -0300

    Adiciona estilos a TaskForm

--- minha-agenda/src/components/TaskForm.tsx
+++ minha-agenda/src/components/TaskForm.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
+// CSS
+import styles from "./TaskForm.module.css";
+
 type Props = {
   btnText: string;
 };
 
 const TaskForm = ({ btnText }: Props) => {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.input_container}>
         <label htmlFor="title">Título:</label>
         <input type="text" name="title" placeholder="Título da tarefa..." />
       </div>
-      <div>
+      <div className={styles.input_container}>
         <label htmlFor="difficulty">Dificuldade:</label>
         <input
           type="text"
